cc0d5aca790acbffa902acc412ed4baf917ce92f
Devi Raju <deviraju999@gmail.com>
2022-09-28 21:24:16 -0700

    added

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
 const wordSearch = (letters, word) => { 
+    
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+    for (let l of horizontalJoin) {
+       if (l.includes(word))  return true
+   }
+    const array = [];
+    letters.forEach((input, i) => {          
+      input.forEach((number, index) => {
+        if (i === 0) {  
+          array[index] = [number]; 
+        } else {
+          array[index].push(number);
+        }
+      });
+    });
+    let verticalJoin = array.map(ls => ls.join(''))
+     for (let m of verticalJoin) {
+       if (m.includes(word)) return true
+   }
+   return false;
+  }
 
 module.exports = wordSearch
